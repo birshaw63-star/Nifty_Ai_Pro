1b3a53c7d26d848a836bd4817750f25e48ba11b5
birshaw63-star <birshaw63@gmail.com>
2026-06-29 16:19:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
-app = FastAPI(title="Nifty AI Pro")
+app = FastAPI(
+    title="Nifty AI Pro",
+    version="1.0.0"
+)
+
+# Static files (CSS, JS)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/", response_class=HTMLResponse)
 def home():
     with open("templates/index.html", "r", encoding="utf-8") as file:
-        return HTMLResponse(file.read())
+        return HTMLResponse(content=file.read())
