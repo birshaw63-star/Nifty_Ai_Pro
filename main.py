4cec5ebd1ffa358baf4372cf0c9693b0de01eb88
birshaw63-star <birshaw63@gmail.com>
2026-06-29 16:10:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+app = FastAPI(title="Nifty AI Pro")
 
 @app.get("/", response_class=HTMLResponse)
 def home():
-    return """
-    <html>
-    <head>
-        <title>Nifty AI Pro</title>
-    </head>
-    <body style="background:#111;color:white;text-align:center;font-family:Arial;padding-top:80px;">
-        <h1>🚀 Nifty AI Pro</h1>
-        <h2>Project Started Successfully</h2>
-    </body>
-    </html>
-    """
+    with open("templates/index.html", "r", encoding="utf-8") as file:
+        return HTMLResponse(file.read())
